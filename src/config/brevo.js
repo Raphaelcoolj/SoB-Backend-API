3838b9ee370b147af5b76235271b4a9b2febb5e2
Emeka <ekehchukwuemeka375@gmail.com>
2026-06-13 03:44:31 -0400

    changed from nodemailer to brevo

---
+++ src/config/brevo.js
@@ -0,0 +1,9 @@
+import * as SibApiV3Sdk from '@getbrevo/brevo'
+
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi()
+apiInstance.setApiKey(
+  SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+)
+
+export default apiInstance
--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,34 +1,24 @@
-import transporter from '../config/nodemailer.js';
+import * as SibApiV3Sdk from '@getbrevo/brevo'
+import apiInstance from '../config/brevo.js'
 
 export const sendEmail = async ({ to, subject, html }) => {
-  await transporter.sendMail({
-    from: `"SoB" <${process.env.MAIL_USER}>`,
-    to,
-    subject,
-    html
-  });
-};
+  const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail()
+
+  sendSmtpEmail.subject = subject
+  sendSmtpEmail.htmlContent = html
+  sendSmtpEmail.sender = { name: 'SoB', email: 'noreply@sobplatform.com' }
+  sendSmtpEmail.to = [{ email: to }]
+
+  try {
+    await apiInstance.sendTransacEmail(sendSmtpEmail)
+  } catch (error) {
+    throw new Error(`Brevo email error: ${error.message}`)
+  }
+}
 
 export const sendBatchEmail = async (recipients, subject, html) => {
-  const BATCH_SIZE = 50;
-  const DELAY_MS = 2000;
-  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-  for (let i = 0; i < recipients.length; i += BATCH_SIZE) {
-    const batch = recipients.slice(i, i + BATCH_SIZE);
-    await Promise.all(
-      batch.map((to) =>
-        transporter.sendMail({
-          from: `"SoB" <${process.env.MAIL_USER}>`,
-          to,
-          subject,
-          html,
-        })
-      )
-    );
-    console.log(`Completed batch ${Math.floor(i / BATCH_SIZE) + 1} (${i + batch.length}/${recipients.length})`);
-    if (i + BATCH_SIZE < recipients.length) await delay(DELAY_MS);
+  for (const to of recipients) {
+    await sendEmail({ to, subject, html })
   }
-
-  console.log('Batch email sending complete.');
-};
+  console.log('Batch email sending complete.')
+}
