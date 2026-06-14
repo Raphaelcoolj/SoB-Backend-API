dea6437be0e2bdc59a524987f99a5e7d0a560c16
Emeka <ekehchukwuemeka375@gmail.com>
2026-06-14 13:33:57 -0400

    new updates

--- src/routes/search.routes.js
+++ src/routes/search.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import * as searchController from "../controllers/search.controller.js";
-import { optionalProtect } from "../middlewares/auth.middleware.js";
+import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/posts", optionalProtect, searchController.searchPosts);
-router.get("/users", optionalProtect, searchController.searchUsers);
+router.get("/posts", protect, searchController.searchPosts);
+router.get("/users", protect, searchController.searchUsers);
 
 export default router;
